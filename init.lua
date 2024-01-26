d2d6e23d1a61b8309f0c341a04724bed96b05959
ahmad-su <ahmad.suhae@gmail.com>
2024-01-26 20:57:25 +0700

    added lualine, removed nvim-devicons, added vim adjustments, organized plugins

--- init.lua
+++ init.lua
@@ -17,14 +17,63 @@ vim.o.shadafile = "/data/data/com.termux/files/home/.config/nvim/shada/main.shad
 
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
-require("lazy").setup({
-	'morhetz/gruvbox',
-	'itchyny/lightline.vim',
-	'jiangmiao/auto-pairs',
-	'sheerun/vim-polyglot',
-	'junegunn/fzf',
-	'preservim/nerdtree',
-	'ryanoasis/vim-devicons',
-	'airblade/vim-gitgutter',
-	'Yggdroot/indentLine'
-})
+require("lazy").setup("plugins")
+
+-- Colorscheme
+vim.cmd([[colorscheme gruvbox]])
+-- Pluginpath
+vim.cmd([[ set runtimepath+=~/.config/nvim/plugins ]])
+
+-- Common setup
+vim.cmd([[
+  set clipboard=unnamedplus
+  set encoding=utf-8
+  set fileencoding=utf-8
+  "scriptencoding utf-8
+  syntax enable
+
+  set noswapfile
+  set wrap
+
+  " Indentation
+  set tabstop=2 softtabstop=2 shiftwidth=2 expandtab
+  set smarttab
+  set autoindent
+  set backspace=indent,eol,start
+
+  " Make tilde of the end of buffer disappeared
+  set fillchars=eob:\ ,vert:▎
+
+  " Highlight the cursor
+  set cursorline
+
+  " Cursor type underlined
+  set guicursor=n-v-c-sm:hor20,i-ci-ve:ver25,r-cr-o:hor20
+
+  " Always show at least three lines above/below the cursor.
+  set scrolloff=3
+  " Always show at least seven line left/right of the cursor.
+  set sidescrolloff=7
+
+  " line numbers
+  set number
+  set relativenumber
+  set numberwidth=4
+
+  " Highlight matching pairs of brackets. Use the '%' character to jump between them.
+  set matchpairs+=<:>
+
+  " Display different types of white spaces.
+  set list
+  set listchars=tab:›\ ,trail:•,extends:#,nbsp:.
+
+  " Remove timeout for partially typed commands
+  set notimeout
+
+  filetype plugin indent on
+  
+  " touch screen support
+  set mouse=a
+
+  
+]])
---
+++ lua/plugins.lua
@@ -0,0 +1,16 @@
+return {
+	'morhetz/gruvbox',
+	'jiangmiao/auto-pairs',
+	'sheerun/vim-polyglot',
+--	'nvim-treesitter/nvim-treesitter',
+	'junegunn/fzf',
+	'preservim/nerdtree',
+	'airblade/vim-gitgutter',
+	'Yggdroot/indentLine',
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = {
+			'nvim-tree/nvim-web-devicons'
+		}
+	}
+}
