e86466943bb932a358b4c9663932d763024f59f6
ahmad-su <ahmad.suhae@gmail.com>
2024-01-30 14:04:42 +0700

    added lsp support

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,5 +13,13 @@ return {
 		dependencies = {
 			'nvim-tree/nvim-web-devicons'
 		}
-	}
+	},
+	'neovim/nvim-lspconfig',
+	'hrsh7th/cmp-nvim-lsp',
+	'hrsh7th/cmp-buffer',
+	'hrsh7th/cmp-path',
+	'hrsh7th/cmp-cmdline',
+	'hrsh7th/nvim-cmp',
+	'hrsh7th/cmp-vsnip',
+	'hrsh7th/vim-vsnip'
 }
